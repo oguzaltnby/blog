41a9aa9851f86db7d705fa5bf4c9d2b2964caddc
Oğuz <oguzhanaltunbey3@gmail.com>
2023-04-12 23:18:09 +0300

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -53,6 +53,12 @@ export default Vue.extend({
             href: "/me/repos",
           },
           {
+            title: "Songs",
+            description:
+              "Trust your taste? Compare your favourite songs and artists with mine!",
+            href: "/me/songs",
+          },
+          {
             title: "Contact",
             description:
               "Need help with anything? Want to get in touch? Send me a message!",
@@ -66,18 +72,7 @@ export default Vue.extend({
               "Will update soon",
             href: "/blog",
           },
-          {
-            title: "Daily Songs",
-            description:
-              "Fresh, cool and great song recommendations from me each day!",
-            href: "/daily",
-          },
-          {
-            title: "Donate",
-            description:
-              "Want to support me and my projects so that I can create better stuff in the future?",
-            href: "/donate",
-          },
+
         ],
       },
       skills: [
