461f2f6d61e815ca2f1fa7e65b0872d6a12efb44
Oğuz <oguzhanaltunbey3@gmail.com>
2023-04-12 23:04:30 +0300

    Update constants.ts

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,8 @@
 export default {
   social: {
     github: "https://github.com/oguzaltnby/",
-    instagram: "",
-    reddit: "",
+    instagram: "https://instagram.com/oguzaltnby/",
+    reddit: "https://www.reddit.com/user/oguzaltnby",
     email: "oguzaltnby@protonmail.com",
   },
 
