b96afed648eed71e0d11fbbc6ff14db344ddcc72
Oğuz <oguzhanaltunbey3@gmail.com>
2023-04-12 21:53:19 +0300

    a
    a

--- config/head.ts
+++ config/head.ts
@@ -1,7 +1,7 @@
 import { NuxtOptionsHead } from "@nuxt/types/config/head"
 
 /* Define constants */
-const image = "https://oguzaltnby.netlify.app/v.png"
+const image = "https://oguzaltnby.netlify.app/logo.png"
 const description =
   "Young information technologies student from Turkey, interested in languages, gaming, skating and programming"
 
