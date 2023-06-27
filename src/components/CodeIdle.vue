bd7a881dc4b85e6ddba221394801ade4a8f326ae
Oğuz <oguzhanaltunbey3@gmail.com>
2023-06-27 13:55:09 +0300

    Update CodeIdle.vue

--- src/components/CodeIdle.vue
+++ src/components/CodeIdle.vue
@@ -1,9 +1,5 @@
 <script lang="ts">
 import Vue from "vue"
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
 
 
 export default Vue.extend({
@@ -20,7 +16,7 @@ export default Vue.extend({
     v-tippy="{
       content: '⌘/Ctrl + K',
     }"
-    icon="code"
+    icon="Menu"
     class="focus:outline-none"
     rounded
     elevated
