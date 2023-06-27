5d74bf0c33b9df5275ddf646f595d0a03c664722
Oğuz <oguzhanaltunbey3@gmail.com>
2023-06-27 15:50:17 +0300

    a

--- src/components/CodeIdle.vue
+++ src/components/CodeIdle.vue
@@ -12,13 +12,9 @@ export default Vue.extend({
 </script>
 
 <template>
-  <Button
-    v-tippy="{
-      content: '⌘/Ctrl + K',
-    }"
-    class="focus:outline-none"
-    rounded
-    elevated
-    @click.native="togglePalette"
-  />
+  <Button v-tippy="{
+    content: '⌘/Ctrl + K',
+  }" class="focus:outline-none" rounded elevated @click.native="togglePalette"> <template #icon>
+      <IconChevron up class="h-4 w-4" />
+    </template> </Button>
 </template>
