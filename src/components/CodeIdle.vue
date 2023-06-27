64e35ae1744fcf00633179c3a487d04635310d7f
Oğuz <oguzhanaltunbey3@gmail.com>
2023-06-27 16:13:56 +0300

    Update CodeIdle.vue

---
+++ src/components/CodeIdle.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from "vue"
+
+
+export default Vue.extend({
+  methods: {
+    togglePalette() {
+      this.$root.$emit("openCommandMenu")
+    },
+  },
+})
+</script>
+
+<template>
+  <Button v-tippy="{
+    content: '⌘/Ctrl + K',
+  }" v-text="{ㅤ}" class="focus:outline-none" rounded elevated @click.native="togglePalette" />
+</template>
