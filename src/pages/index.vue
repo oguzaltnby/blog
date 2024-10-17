f182bc61408fe1b058f40c31b5eec83ca44b22cd
Oğuz <oguzhanaltunbey3@gmail.com>
2024-10-17 22:03:14 +0300

    Update Button class in index.vue for improved padding

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -215,7 +215,7 @@ export default Vue.extend({
             <Button
               v-for="item in ['TypeScript', 'Javascript', 'C#']"
               :key="item"
-              class="inline-block flex items-center justify-center text-center px-4 py-2"
+              class="inline-block flex items-center justify-center text-center px-6 py-2"
             >
               <IconDev :brand="item" class="h-5 w-5" />
               <span class="ml-2">{{ item }}</span>
