69a88e5f80803f840008eeda36a36a6123325ca4
Oğuz <oguzhanaltunbey3@gmail.com>
2024-10-18 21:42:20 +0300

    Update heading in index.vue for improved responsiveness

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -199,7 +199,7 @@ export default Vue.extend({
 
             <h1
               class="font-semibold text-center text-4xl md:(text-6xl text-left) text-black/90 leading-normal dark:text-white/90">
-              Full-stack web developer
+              Full-stack web<br class="md:hidden" />developer
             </h1>
           </div>
 
