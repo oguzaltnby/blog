f51c54df92e94327a7177c197c57fea104fcb0d7
Oğuz <oguzhanaltunbey3@gmail.com>
2024-10-18 22:12:09 +0300

    Fix tooltip content binding in index.vue for improved functionality

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -205,7 +205,7 @@ export default Vue.extend({
 
           <div class="flex items-center justify-center md:justify-start gap-x-3 gap-y-2 flex-wrap">
             <Button v-for="item in ['TypeScript', 'Javascript', 'C#']" :key="item" class="inline-block" v-tippy="{
-              content: '{{ item }}',
+              content: item, placement: 'bottom',
             }">
               <IconDev :brand="item" class="h-5 w-5" />
             </Button>
