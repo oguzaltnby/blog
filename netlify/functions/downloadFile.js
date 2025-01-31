7a72b992b4846ffa8effbce19f96814ecad6b1e2
oguzhan <oguzhanaltunbey3@gmail.com>
2025-01-31 19:47:47 +0300

    Move example.txt to static directory and update file paths in downloadFile and getFileContent functions

--- netlify/functions/downloadFile.js
+++ netlify/functions/downloadFile.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-  const filePath = path.join(process.cwd(), 'files/example.txt'); // Dosya yolunu belirtin
-  console.log('Current working directory:', process.cwd());
-  console.log('File path:', filePath);
-
+  const filePath = path.join(process.cwd(), 'static/files/example.txt'); // Dosya yolunu belirtin
   try {
     const data = fs.readFileSync(filePath);
     return {
@@ -18,7 +15,6 @@ exports.handler = async function(event, context) {
       isBase64Encoded: true,
     };
   } catch (error) {
-    console.error('Error reading file:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Dosya indirilemedi' }),
